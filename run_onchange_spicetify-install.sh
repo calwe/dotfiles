645596313a53c6ec57dd57aa92c47cbccd519309
Callum Webb <callum.s.webb@gmail.com>
2025-12-24 14:16:04 +0000

    remove spotify, update gitconfig

--- run_onchange_spicetify-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-spicetify config inject_css 1
-spicetify config replace_colors 1
-spicetify config current_theme marketplace
-
-spicetify config custom_apps marketplace
-spicetify backup apply
